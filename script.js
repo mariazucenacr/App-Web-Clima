58e2e92b598b08626365624516328216eee1b5fb
Mariazucena Calderòn Riera <107963749+mariazucenacr@users.noreply.github.com>
2024-09-12 18:22:32 -0500

    Se creo la funcionalidad de la aplicacion web en javascript

---
+++ script.js
@@ -0,0 +1,57 @@
+//let api_key = //aqui se pone el API UNICO QUE TE DA openweathermap
+let urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+//como la api del clima trae en Kelvin debemos restar el valor para la transformacion en grados celius
+let difKelvin = 273.15
+
+//creamos el evento de "escucha" para el boton de busqueda
+document.getElementById('botonBusqueda').addEventListener('click', () => {
+    //crearemos la variable dinamica para el nombre de la ciudad
+    const ciudad = document.getElementById('ciudadEntrada').value
+    if (ciudad) {
+        fetchDatosClima(ciudad)
+    }
+})
+
+function fetchDatosClima(ciudad) {
+    fetch(`${urlBase}?q=${ciudad}&appid=${api_key}`)
+        .then(data => data.json())
+        .then(data => mostrarDatosClima(data))
+}
+
+function mostrarDatosClima(data) {
+    console.log(data)
+    const divDatosClima = document.getElementById('datosClima')
+    divDatosClima.innerHTML = ''
+
+    const ciudadNombre = data.name
+    const paisNombre = data.sys.country
+    const temperatura = data.main.temp
+    const humedad = data.main.humidity
+    const descripcion = data.weather[0].description
+    const icono = data.weather[0].icon
+
+    //vamos a crear elementos para mostrar
+    const ciudadTitulo = document.createElement('h2')
+    ciudadTitulo.textContent = `${ciudadNombre}, ${paisNombre}`
+
+    const temperaturaInfo = document.createElement('p')
+    temperaturaInfo.textContent = `La temperatura es: ${Math.floor(temperatura - difKelvin)}°C`
+
+    const humedadInfo = document.createElement('p')
+    humedadInfo.textContent = `La humedad es: ${humedad}%`
+
+    const iconoInfo = document.createElement('img')
+    iconoInfo.src =`https://openweathermap.org/img/wn/${icono}@2x.png`
+
+    const descripcionInfo = document.createElement('p')
+    descripcionInfo.textContent = `La descripcion meteorologica es: ${descripcion}`
+
+    //vamos a agregar estos elementos dentro del DIV con appendChild
+    divDatosClima.appendChild(ciudadTitulo)
+    divDatosClima.appendChild(temperaturaInfo)
+    divDatosClima.appendChild(humedadInfo)
+    divDatosClima.appendChild(iconoInfo)
+    divDatosClima.appendChild(descripcionInfo)
+
+
+}
